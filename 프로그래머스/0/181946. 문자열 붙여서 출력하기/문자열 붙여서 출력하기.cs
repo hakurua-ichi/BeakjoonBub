82225071c199175bda97cd82c22da6df3e62cdff
SangHyeon Jeong <jjxx11301@gmail.com>
2025-03-29 16:08:57 +0900

    [level 0] Title: 문자열 붙여서 출력하기, Time: 18.26 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181946. 문자열 붙여서 출력하기/문자열 붙여서 출력하기.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Example
+{
+    public static void Main()
+    {
+        String input;
+
+        Console.Clear();
+        input = Console.ReadLine();
+        Console.WriteLine(input.Replace(" ",""));
+
+    }
+}
